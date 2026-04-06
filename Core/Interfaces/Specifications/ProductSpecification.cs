ad45e2632ac432d72cf7f209db3a6cab64d3ac0d
ภาคภูมิ จันทร์ทึก <pakphoom44aa@gmail.com>
2026-04-06 17:44:35 +0700

    Section04 : Creating product spec parameters & Search

--- Core/Interfaces/Specifications/ProductSpecification.cs
+++ Core/Interfaces/Specifications/ProductSpecification.cs
@@ -4,27 +4,26 @@ namespace Core.Specifications;
 
 public class ProductSpecification : BaseSpecification<Product>
 {
-    public ProductSpecification(string? brand, string? type, string? sort)
+    //base(x =>) ????????????????? Constructor BaseSpecification
+    public ProductSpecification(ProductSpecParams productParams)
         : base(x =>
-            (string.IsNullOrWhiteSpace(brand) || x.Brand == brand) &&
-            (string.IsNullOrWhiteSpace(type) || x.Type == type))
+            (string.IsNullOrEmpty(productParams.Search)
+                || x.Name.ToLower().Contains(productParams.Search)) &&
+            (!productParams.Brands.Any() || productParams.Brands.Contains(x.Brand)) &&
+            (!productParams.Types.Any() || productParams.Types.Contains(x.Type)))
     {
 
-        if (!string.IsNullOrWhiteSpace(sort))
+        switch (productParams.Sort)
         {
-            switch (sort)
-            {
-                case "priceAsc":
-                    AddOrderBy(x => x.Price);
-                    break;
-                case "priceDesc":
-                    AddOrderByDescending(x => x.Price);
-                    break;
-                default:
-                    AddOrderBy(x => x.Name);
-                    break;
-            }
-
+            case "priceAsc":
+                AddOrderBy(x => x.Price);
+                break;
+            case "priceDesc":
+                AddOrderByDescending(x => x.Price);
+                break;
+            default:
+                AddOrderBy(x => x.Name);
+                break;
         }
     }
 }
