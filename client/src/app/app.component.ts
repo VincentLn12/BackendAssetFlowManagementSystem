cfe9b916853b37f2f2f1737e354ac38d7bc262a5
ภาคภูมิ จันทร์ทึก <pakphoom44aa@gmail.com>
2026-04-08 16:52:46 +0700

    End of Section06 : Making http requests in Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from './layout/header/header.component';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +13,15 @@ import { HeaderComponent } from './layout/header/header.component';
 })
 export class AppComponent {
   title = 'Skinet';
+  baseUrl = environment.baseUrl;
+  private http = inject(HttpClient);
+  products: Product[] = [];
+
+  ngOnInit() {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (response) => (this.products = response.data),
+      error: (error) => console.error(error),
+      complete: () => console.log('complete'),
+    });
+  }
 }
