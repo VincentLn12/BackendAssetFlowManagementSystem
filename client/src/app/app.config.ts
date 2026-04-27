d1bcb055e263f9aabce8ccbc50178f4156330079
ภาคภูมิ จันทร์ทึก <pakphoom44aa@gmail.com>
2026-04-27 11:06:22 +0700

    Section09 : (client) Designing the cart

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import {
   ApplicationConfig,
+  inject,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
@@ -8,8 +10,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error-interceptor';
-import { loadingInterceptor } from './core/interceptors/loading-interceptor';
 import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { loadingInterceptor } from './core/interceptors/loading-interceptor';
+import { InitService } from './core/services/init.service';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +21,15 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+      return lastValueFrom(initService.init()).finally(() => {
+        const splash = document.getElementById('initial-splash');
+        if (splash) {
+          splash.remove();
+        }
+      });
+    }),
     {
       provide: MAT_DIALOG_DEFAULT_OPTIONS,
       useValue: { autoFocus: 'dialog', restoreFocus: true },
--- client/src/app/core/services/init.service.ts
+++ client/src/app/core/services/init.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { CartService } from './cart.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InitService {
-  
+  private cartService = inject(CartService);
+
+  init() {
+    const cartId = localStorage.getItem('cart_id');
+    const cart$ = cartId ? this.cartService.getCart(cartId) : of(null);
+    console.log(cartId);
+    console.log(cart$);
+    return cart$;
+  }
 }
