aecbc0df2aaca1839c1e8b0ae5e7e79bbf2f1159
ภาคภูมิ จันทร์ทึก <pakphoom44aa@gmail.com>
2026-04-27 14:24:04 +0700

    End of Section09 : (client) Adding order summary & cart update to product detail

--- client/src/app/shared/components/order-summary/order-summary.component.ts
+++ client/src/app/shared/components/order-summary/order-summary.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../../core/services/cart.service';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-summary',
-  imports: [],
+  imports: [
+    MatButton,
+    RouterLink,
+    CurrencyPipe
+  ],
   templateUrl: './order-summary.component.html',
-  styleUrl: './order-summary.component.scss',
+  styleUrl: './order-summary.component.scss'
 })
-export class OrderSummaryComponent {}
+export class OrderSummaryComponent {
+  cartService = inject(CartService);
+}
