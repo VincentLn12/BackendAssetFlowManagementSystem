5e7e13e5c306cd9f52c0ebbba87367935de09c7f
ภาคภูมิ จันทร์ทึก <pakphoom44aa@gmail.com>
2026-04-29 08:56:45 +0700

    Section11 Client-Identity : Creating related components

---
+++ client/src/app/shared/components/text-input/text-input.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-input',
+  imports: [],
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.scss',
+})
+export class TextInputComponent {}
