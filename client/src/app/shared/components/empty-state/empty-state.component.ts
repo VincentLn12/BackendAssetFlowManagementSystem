80daa31dadb1ac84695bfefe0ac543d7c3ecd06d
ภาคภูมิ จันทร์ทึก <pakphoom44aa@gmail.com>
2026-04-29 10:39:51 +0700

    End of Section11 : Adding auth & empty cart guards

--- client/src/app/shared/components/empty-state/empty-state.component.ts
+++ client/src/app/shared/components/empty-state/empty-state.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-empty-state',
-  imports: [],
+  imports: [
+    MatIcon, 
+    MatButton,
+    RouterLink 
+  ],
   templateUrl: './empty-state.component.html',
-  styleUrl: './empty-state.component.scss',
+  styleUrl: './empty-state.component.scss'
 })
-export class EmptyStateComponent {}
+export class EmptyStateComponent {
+
+}
