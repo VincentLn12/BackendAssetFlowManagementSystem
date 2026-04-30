4412fef9c883343a017f8d6c588483497db8f71b
ภาคภูมิ จันทร์ทึก <pakphoom44aa@gmail.com>
2026-04-30 11:45:31 +0700

    Section14 :  Creating the order components & related components

---
+++ client/src/app/features/orders/order-detailed/order-detailed.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-detailed',
+  imports: [],
+  templateUrl: './order-detailed.component.html',
+  styleUrl: './order-detailed.component.scss',
+})
+export class OrderDetailedComponent {}
