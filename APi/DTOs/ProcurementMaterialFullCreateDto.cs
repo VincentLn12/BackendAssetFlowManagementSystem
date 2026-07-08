e9ece622a0e756f00df6c36bf9dda4f3fbd4cc77
ภาคภูมิ จันทร์ทึก <pakphoom44aa@gmail.com>
2026-07-08 09:42:47 +0700

    8/7

---
+++ APi/DTOs/ProcurementMaterialFullCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace APi.DTOs
+{
+    public class ProcurementMaterialFullCreateDto
+    {
+        public ProcurementRecordCreateDto procurement_record { get; set; } = new();
+
+        public List<MaterialReceiveDetailDto> material_receive_details { get; set; } = new();
+    }
+}
